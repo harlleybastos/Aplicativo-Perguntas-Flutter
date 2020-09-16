66bc28a93fdcf02cd4529ac938d3f030d97d96ce
Harlley Bastos <harlleybastos@hotmail.com>
2020-09-16 05:57:31 -0300

    nova funcionalidade

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,57 @@
-
 import 'package:flutter/material.dart';
 
-main(){
-  runApp(new PerguntaApp());
+main() => runApp(PerguntaApp());
 
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+
+    print(perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final perguntas = [
+      'Qual é a sua cor favorita ?',
+      'Qual é o seu animal favorito?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        //Estrtutura da aplicação
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            // Lista de vários componentes
+            Text(perguntas[perguntaSelecionada]), // Caixa de Texto
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class PerguntaApp extends StatelessWidget {
-  Widget build (BuildContext context){
-    return MaterialApp(
-      home: Text('Olá Flutter !'),
-    );
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
